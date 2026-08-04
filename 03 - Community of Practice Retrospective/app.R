8fdccc4430b3042475933344daae82859b23188d
Ava Hoffman <avamariehoffman@gmail.com>
2026-08-04 14:12:53 -0400

    Add survey

--- 03 - Community of Practice Retrospective/app.R
+++ 03 - Community of Practice Retrospective/app.R
@@ -35,7 +35,9 @@ ui <- sd_ui()
 
 server <- function(input, output, session) {
   # Define any conditional skip logic here (skip to page if a condition is true)
-  sd_skip_if()
+  sd_skip_if(
+    input$consent == "no" ~ "ineligible"
+  )
 
   # Define any conditional display logic here (show a question if a condition is true)
   sd_show_if()
